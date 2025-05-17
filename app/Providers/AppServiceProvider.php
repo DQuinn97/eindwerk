8f12bb6297de54e21860ab7f7ad0e98b186db838
DQuinn97 <quinten2407@gmail.com>
2025-05-17 14:06:38 +0200

    assets on https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Permission;
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -20,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if (env('APP_ENV') !== 'local') {
+            $url->forceScheme('https');
+        }
         // $hasPermissionTable = Schema::hasTable('connections');
         // $hasUserTable = Schema::hasTable('users');
         // $hasRoleTable = Schema::hasTable('roles');
